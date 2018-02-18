485c1cf3ae5b162bf58704fedab4409a03c16f3f
Davis Ford <ford_davis@bah.com>
2018-02-17 22:14:50 -0500

    spit simple logic out of handler

--- handler.py
+++ handler.py
@@ -7,91 +7,13 @@ import json
 import logging
 import traceback
 
-from svgpathtools import parse_path
-
 from colors import DEFAULT_COLORS
-from custom_svg import davis_disvg
-from parse_sentences import split_into_sentence_lengths
 from s3 import is_duplicate_checksum
+from svg_simple import save_simple_xml_to_s3
 from svg_split import save_split_xml_to_s3
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-def filter_length(list_of_sentence_lists):
-    return [len(x) for x in list_of_sentence_lists]
-
-
-def get_sentence_lengths(input_text):
-    """
-
-    :param input_text: str
-    :return:
-    """
-    sentence_lengths = split_into_sentence_lengths(input_text)
-    return sentence_lengths
-
-
-def plot_lengths(array_of_ints):
-    # Turn left 90 degrees each time
-    behavior_ref = ['h -', 'v ', 'h ', 'v -']
-    path_str = 'M50 20j'
-    count = 0
-    for num in array_of_ints:
-        move = '%s%s' % (behavior_ref[count], num)
-        path_str = ' '.join([path_str, move])
-        count = 0 if count == 3 else count + 1
-
-    return path_str
-
-
-def get_simple_preparsed_paths(simple_path):
-    return parse_path(simple_path)
-
-
-def get_paths(text):
-    """
-
-    :param text:
-    :return:
-    """
-    sentence_lengths = get_sentence_lengths(text)
-    path_str = plot_lengths(sentence_lengths)
-    return parse_path(path_str)
-
-
-def save_xml_to_s3(json_obj):
-    """
-
-    :param json_obj:
-    :return: str
-    """
-    try:
-        if json_obj['simple_path'] is not None and len(json_obj['simple_path']) > 0:
-            logger.info('Using simple_path parameter')
-            paths = get_simple_preparsed_paths(json_obj['simple_path'])
-        else:
-            logger.info('Using simple get_paths calculations')
-            paths = get_paths(json_obj['text'])
-
-        if json_obj['node_colors'] is not None:
-            node_opts = {'node_colors': json_obj['node_colors'], 'node_radii': [1, 1]}
-        else:
-            node_opts = {'node_colors': None, 'node_radii': [0, 0]}
-
-        url = davis_disvg(
-            paths=paths,
-            colors=[json_obj['color']],
-            nodes=[paths.point(0.0), paths.point(1.0)],
-            checksum=json_obj['checksum'],
-            bg_color=json_obj['bg_color'],
-            **node_opts
-        )
-        return url
-    except Exception as e:
-        traceback.print_exc()
-        return 'Error building xml_string: %s' % str(e)
 
 
 def satisfies_split_conditions(json_obj):
@@ -176,7 +98,7 @@ def endpoint(event, context):
             url = save_split_xml_to_s3(json_obj)
             response['body'] = json.dumps({'s3_url': url})
         else:
-            url = save_xml_to_s3(json_obj)
+            url = save_simple_xml_to_s3(json_obj)
             response['body'] = json.dumps({'s3_url': url})
 
         logger.info('Endpoint Response:')
